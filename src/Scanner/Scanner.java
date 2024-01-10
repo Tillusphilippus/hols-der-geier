1fd23733be2460ae085b177bc09548d6f440ab9b
Tillusphilippus <155558200+Tillusphilippus@users.noreply.github.com>
2024-01-10 11:42:17 +0100

    Version 2.0

---
+++ src/Scanner/Scanner.java
@@ -0,0 +1,11 @@
+package Scanner;
+
+/**
+ * Die Klasse Scanner ist dafür zuständig nach jeder Runde den Gegner zu analysieren und eine passende Gegenstrategie zu wählen.
+ *
+ * @author Till N.
+ * @version 0.1
+ */
+
+public class Scanner {
+}
